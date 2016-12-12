f8cb669e796fc2ea4fb607bf3b749b3a9a882c87
mpastorperez <mpastorperez@atanasoff31.danielcastelao.org>
2016-12-12 09:19:40 +0100

    modificacion, muestro

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -7,11 +7,12 @@ public class Factorial {
 
     public static void main(String[] args) {
 
-    //Poner las variables del mismo tipo juntas
-    int j,f,i;
+ //Poner las variables del mismo tipo juntas
  //Muevo todo a una clase y le doy valores a las variables para usar la main para mostrar
-    //Las variables tenian los valores en el programa asique se los asigno
-
+//Las variables tenian los valores en el programa asique se los asigno
+        Calculos calc=new Calculos();
+        calc.condicion();
+//Finalmente muestro lo de la clase llamando al metodo
 
 
     }
